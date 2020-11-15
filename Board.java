f20488c1cd97d3fbfe4a4dbfa327e5451ac6c4bd
gabrieldev525 <gvictor525.gv@gmail.com>
2020-11-15 00:52:31 -0300

    Base of generate board method

--- Board.java
+++ Board.java
@@ -7,10 +7,22 @@ public class Board {
 
     private Ship[] SHIP_TYPES = new Ship[Constants.SHIPS_CONFIG.length];
 
+    // storage the count of ship type in the board
+    private int[] BOARD_SHIP_TYPES_COUNT;
+
     public Board(int size) {
         this.size = size;
 
+        // get all types possibles of ships
         SHIP_TYPES = Utils.getShipsTypes();
+
+        // initialize the variable with 0
+        BOARD_SHIP_TYPES_COUNT = new int[SHIP_TYPES.length];
+        for(int i = 0; i < SHIP_TYPES.length; i++) {
+            BOARD_SHIP_TYPES_COUNT[i] = 0;
+        }
+
+        board = new Position[size * size];
     }
 
     public Board() {}
@@ -24,9 +36,60 @@ public class Board {
     }
 
     public void generate() {
-        // delete a old data file
+        // delete old data file
         Utils.deleteFolder("./data");
 
         Random random = new Random();
+
+        for(Ship currentShip : SHIP_TYPES) {
+            for(int i = 0; i < currentShip.getCount(); i++) {
+                boolean canAddShip = true;
+
+                do {
+                    // if int random was 0, the ship will be add horizontally however, will be added vertically
+                    boolean horizontal = random.nextInt(1) == 0;
+
+                    /**
+                     * remove the size of the ship from the possible starting positions depending on the
+                     * direction, in order to have space in relation to the edge of the board
+                     */
+                    int sizeX, sizeY;
+                    sizeX = sizeY = size;
+                    if(horizontal)
+                        sizeX -= currentShip.getSize();
+                    else
+                        sizeY -= currentShip.getSize();
+
+                    int randomX = random.nextInt(sizeX);
+                    int randomY = random.nextInt(sizeY);
+
+                    for(Position pos : board) {
+                        if(pos != null) {
+                            if(horizontal)
+                                if(pos.getX() >= randomX && pos.getX() <= randomX + currentShip.getSize())
+                                    canAddShip = false;
+                            else
+                                if(pos.getY() >= randomY && pos.getY() <= randomY + currentShip.getSize())
+                                    canAddShip = false;
+                        }
+
+                        if(!canAddShip)
+                            break;
+                    }
+
+                    // add the ship to the board
+                    if(canAddShip) {
+                        for(int j = 0; j < currentShip.getSize(); j++) {
+                            int x = horizontal ? randomX + j : randomX;
+                            int y = horizontal ? randomY : randomY + j;
+                            Position pos = new Position(currentShip, x, y);
+                            board[(x * y)] = pos;
+                        }
+                    }
+                } while(!canAddShip);
+            }
+        }
+
+        System.out.println("BOARD GENERATE SUCCESSFULLY");
     }
 }

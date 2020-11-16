1b7dbaf58276d3777c3aec9fba59c9664908abf4
gabrieldev525 <gvictor525.gv@gmail.com>
2020-11-16 08:40:51 -0300

    Remove comments

--- Constants.java
+++ Constants.java
@@ -6,9 +6,9 @@ public abstract class Constants {
      * Structure: { id, name, size, count }
      */
     public static Object[][] SHIPS_CONFIG = {
-        {1, "Destroyers", 3, 3},  // 10, 11, 12
-        {2, "Tankers", 4, 2}, // 20, 21
-        {3, "Submarine", 2, 4}, // 30, 31, 32, 33
-        {4, "Aircraft Carrier", 5, 1}, // 40
+        {1, "Destroyers", 3, 3},
+        {2, "Tankers", 4, 2},
+        {3, "Submarine", 2, 4},
+        {4, "Aircraft Carrier", 5, 1},
     };
 }
